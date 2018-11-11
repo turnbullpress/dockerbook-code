df936a76a46be67b75447d80046e7a842b3a54e2
James Turnbull <james@lovedthanlost.net>
2018-11-12 06:48:32 +1100

    Updated node app

--- code/6/node/nodejs/nodeapp/server.js
+++ code/6/node/nodejs/nodeapp/server.js
@@ -1,27 +1,31 @@
 var fs = require('fs');
 var express = require('express'),
+    session = require('express-session')
+    cookieParser = require('cookie-parser')
+    morgan = require('morgan')
     app = express(),
     redis = require('redis'),
-    RedisStore = require('connect-redis')(express),
+    RedisStore = require('connect-redis')(session),
     server = require('http').createServer(app);
 
 var logFile = fs.createWriteStream('/var/log/nodeapp/nodeapp.log', {flags: 'a'});
 
-app.configure(function() {
-  app.use(express.logger({stream: logFile}));
-  app.use(express.cookieParser('keyboard-cat'));
-  app.use(express.session({
+app.use(morgan('combined', {stream: logFile}));
+app.use(cookieParser('keyboard-cat'));
+app.use(session({
+        resave: false,
+        saveUninitialized: false,
         store: new RedisStore({
             host: process.env.REDIS_HOST || 'redis_primary',
             port: process.env.REDIS_PORT || 6379,
             db: process.env.REDIS_DB || 0
         }),
+        secret: 'keyboard cat',
         cookie: {
             expires: false,
             maxAge: 30 * 24 * 60 * 60 * 1000
         }
-    }));
-});
+}));
 
 app.get('/', function(req, res) {
   res.json({
